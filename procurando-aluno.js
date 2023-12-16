e3bb3bacf86bf14b6729b989395a5986222e10e7
fmauriccio <88916124+fmauriccio@users.noreply.github.com>
2023-12-16 19:01:28 -0300

    AULA 03

---
+++ procurando-aluno.js
@@ -0,0 +1,27 @@
+// Arquivo procurando-aluno.js
+
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+  if (listaDeAlunosEMedias[0].includes(aluno)) {
+    //const alunos = listaDeAlunosEMedias[0];
+    //const medias = listaDeAlunosEMedias[1];
+
+    const [alunos, medias] = listaDeAlunosEMedias;
+
+    console.log(`${aluno} está cadastrado!`);
+
+    const indice = alunos.indexOf(aluno);
+
+    const mediaDoAluno = medias[indice];
+
+    console.log(`${aluno} tem a média ${mediaDoAluno}.`);
+  } else {
+    console.log("Aluno não encontrado!");
+  }
+}
+
+exibeNomeENota("João");
