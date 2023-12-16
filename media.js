09e4f74487ec1fad183299e1ce5727df1f910edc
fmauriccio <88916124+fmauriccio@users.noreply.github.com>
2023-12-16 19:00:40 -0300

    AULA 01

---
+++ media.js
@@ -0,0 +1,17 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// posições:
+//              0   1   2   3
+const notas = [10, 5.6, 8, 7.5]
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
+
+console.log(notas[0]);
+console.log(notas[1]);
+console.log(notas[2]);
+console.log(notas[3]);
